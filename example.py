71b0fdf07565b90b7703ad3eefad999d72fb66fc
A.J. Stein <alexander.stein@nist.gov>
2023-04-21 11:19:52 -0400

    Configure secrets in workflow.

--- example.py
+++ example.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env
+import hashlib
+import sys
 
 if __name__ == '__main__':
-    print('Hello world, from GitHub Actions!')
+    if not len(sys.argv) == 3:
+        print('bad')
+
+    secret, expected_hash = sys.argv[1], sys.argv[2]
+    sha256 = hashlib.sha256()
+    sha256.update(secret.encode('utf-8'))
+    actual_hash = sha256.hexdigest()
+
+    if actual_hash == expected_hash: print('good')
+    else: print('bad')
